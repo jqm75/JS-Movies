5534417ecf1d24fefeb8044c85797c05b56e90d7
Quim <joaquimpujol@gmail.com>
2022-10-18 12:58:45 +0200

    Inicio E1

--- src/films.js
+++ src/films.js
@@ -1,6 +1,6 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
-  let result = 0;
+  let result = [];
   console.log("EXERCICE 1 ->", result);
   return result;
 }
